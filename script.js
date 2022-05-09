f4ab4c99b761f3382325fe1de544dff2658101a2
Billy Ridgeway <cachaco@gmail.com>
2022-05-09 10:35:33 +0800

    add user input via prompt

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 "use strict";
 
-const jonasArray = [
-  "Jonas",
-  "Schmedtmann",
-  2037 - 1991,
-  "teacher",
-  ["Michael", "Peter", "Steven"],
-];
+// const jonasArray = [
+//   "Jonas",
+//   "Schmedtmann",
+//   2037 - 1991,
+//   "teacher",
+//   ["Michael", "Peter", "Steven"],
+// ];
 
 const jonas = {
   firstName: "Jonas",
@@ -15,3 +15,45 @@ const jonas = {
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
 };
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@jonasschmedtman";
+
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+
+// console.log(jonas["first" + nameKey]);
+// console.log(jonas["last" + nameKey]);
+
+// const interestedIn = prompt(
+//   "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends."
+// );
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log(
+//     `Wrong request! Choose between firstName, lastName, age, job, and friends.`
+//   );
+// }
+
+const bestFriend = prompt(
+  "Who is Jonas' best friend? Choose from Michael, Peter or Steven."
+);
+
+if (
+  bestFriend === "Michael" ||
+  bestFriend === "Peter" ||
+  bestFriend === "Steven"
+) {
+  console.log(
+    ` ${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${bestFriend}.`
+  );
+} else {
+  console.log(
+    `Wrong input! ${bestFriend} is not one of Jonas' friends. Please choose between Michael, Peter or Steven.`
+  );
+}
